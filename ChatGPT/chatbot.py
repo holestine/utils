be430095563c13b1bf5040db6d1b399a10f4b51d
Brian Holestine <holestine@gmail.com>
2023-10-04 09:44:55 -0700

    Buildbox demo

--- ChatGPT/chatbot.py
+++ ChatGPT/chatbot.py
@@ -1,11 +1,11 @@
 import openai
-import key
+import key     # Put the code to load your key in here
 
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
+        temperature=temperature, # Degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
 
@@ -14,45 +14,19 @@ def collect_messages(prompt):
     response = get_completion_from_messages(context) 
     context.append({'role':'assistant', 'content':f"{response}"})
     return response
-    
+
 context = [ {'role':'system', 'content':"""
-You are an automated service to collect orders for a pizza restaurant. \
-You first greet the customer, then collect the order, and then ask if it's a pickup or delivery. \
-You wait to collect the entire order, then summarize it and check for a final time if the customer wants to add anything else. \
-If it's a delivery, you ask for an address. \
-Finally you collect the payment.\
-Make sure to clarify all options, extras and sizes to uniquely identify the item from the menu.\
-You respond in a short, very conversational friendly style. \
-The menu includes:
-pepperoni pizza  12.95, 10.00, 7.00 \
-cheese pizza   10.95, 9.25, 6.50 \
-veggie pizza   11.95, 9.75, 6.75 \
-fries 4.50, 3.50 \
-greek salad 7.25 \
-Toppings: \
-extra cheese 2.00, \
-mushrooms 1.50 \
-sausage 3.00 \
-canadian bacon 3.50 \
-AI sauce 1.50 \
-peppers 1.00 \
-Drinks: \
-coke 3.00, 2.00, 1.00 \
-sprite 3.00, 2.00, 1.00 \
-bottled water 5.00 \
-"""} ]  # accumulate messages
+You are a code generation tool used to create styles for the BuildBox application. \
+The user will give you a description of the style and you will create JSON code. \
+All the properties are the same as XAML styles but there is also a new one, special_property, \
+that should be set to a color that compliments the background. \
+"""} ]  # accumulate messages in this object
 
-messages =  context.copy()
-messages.append(
-{'role':'system', 'content':'create a json summary of the previous food order. Itemize the price for each item\
- The fields should be 1) pizza, include size 2) list of toppings 3) list of drinks, include size   4) list of sides include size  5)total price '},    
-)
- #The fields should be 1) pizza, price 2) list of toppings 3) list of drinks, include size include price  4) list of sides include size include price, 5)total price '},    
+#context.append(
+#{'role':'system', 'content':'Additional information including samples can be added here'},    
+#)
 
-gpt_response = get_completion_from_messages(messages, temperature=0)
-human_response = 'ok'
-gpt_response = collect_messages(human_response)
+human_response = input("\nStart using the BuildBox Styles Chatbot.\n\n")
 while human_response != 'done':
-    human_response = input(f"{gpt_response}\n")
     gpt_response   = collect_messages(human_response)
-
+    human_response = input(f"\n{gpt_response}\n\n")
